64fb05732182f068abd3f063dfdc7f86c989543b
沈佳杰 <shenjiajie@shenjiajiedeMacBook-Air.local>
2018-08-31 19:06:20 +0800

    增加获取到的数据movie字段

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -10,11 +10,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     const movieId = options.movieId
     const url = app.globalData.g_doubanBase + "/v2/movie/subject/" + movieId
 
@@ -22,15 +22,44 @@ Page({
   },
 
   // 发起请求
-  _getMovieDetailInfo(url){
+  _getMovieDetailInfo(url) {
     fly.get(url)
-      .then((res)=>{
+      .then((res) => {
+        this.processData(res)
         console.log(res)
+        console.log(this.data)
       })
-      .catch((err)=> {
+      .catch((err) => {
         console.log(err)
       });
-  }
+  },
+
+  processData(res) {
+    const datas = res.data
+
+    const movie = {
+      movieImg: datas.images ? datas.images.large : '',
+      country: datas.countries[0],
+      stars: util.starNumber(datas.rating.stars),
+      title: datas.title,
+      wishCount: datas.wish_count,
+      commentCount: datas.comments_count,
+      year: datas.year,
+      genres: datas.genres.join("、 "),
+      originalTitle: datas.original_title,
+      averageScore: this._repair(datas.rating.average),
+      summary: datas.summary
+    }
+
+    this.setData({
+      movie
+    })
+  },
+
+  // averageScore补零
+  _repair(num) {
+    return num.toString().length === 1 ? num + '.0' : num
+  },
 
 
 })
